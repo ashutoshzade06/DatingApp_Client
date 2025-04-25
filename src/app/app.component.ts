5dd675cba1ec7038a578e5a4cbd840f3a29dcf8f
Ashutosh Zade <ashutoshzade06@gmail.com>
2025-04-25 17:31:16 +0530

    added routes,guards,theming

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { HomeComponent } from "./home/home.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgFor, NavComponent, HomeComponent],
+  imports: [RouterOutlet,  NavComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
