f6ffc66827ae0e46eacab54b828248de058fd1e9
Ashutosh Zade <ashutoshzade06@gmail.com>
2025-04-28 13:11:27 +0530

    implementation of ErrorHandlers,interceptor

---
+++ src/app/testerrors/testerrors.component.ts
@@ -0,0 +1,52 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+
+@Component({
+  selector: 'app-testerrors',
+  standalone: true,
+  imports: [],
+  templateUrl: './testerrors.component.html',
+  styleUrl: './testerrors.component.css'
+})
+export class TesterrorsComponent {
+private http=inject(HttpClient);
+baseUrl='https://localhost:5001/api/';
+validationErrors:string[]=[];
+
+get400Error(){
+  this.http.get(this.baseUrl+'buggy/bad-request').subscribe({
+    next:response=>console.log(response),
+    error:error=>console.log(error)
+  })
+}
+
+get401Error(){
+  this.http.get(this.baseUrl+'buggy/auth').subscribe({
+    next:response=>console.log(response),
+    error:error=>console.log(error)
+  })
+}
+get404Error(){
+  this.http.get(this.baseUrl+'buggy/not-found').subscribe({
+    next:response=>console.log(response),
+    error:error=>console.log(error)
+  })
+}
+
+get500Error(){
+  this.http.get(this.baseUrl+'buggy/server-error').subscribe({
+    next:response=>console.log(response),
+    error:error=>console.log(error)
+  })
+}
+get400ValidationError(){
+  this.http.post(this.baseUrl+'account/register',{}).subscribe({
+    next:response=>console.log(response),
+    error:error=>{console.log(error);
+      this.validationErrors=error;
+    }
+  })
+}
+
+
+}
